991e684e6472b49df38a9c415cd00bfd09151852
cdisciascio <cecidisi@gmail.com>
2015-06-30 13:17:29 +0200

    working on form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+(function(){
+
+var $inputPanels = $('.input-panel');
+
+
+
+
+
+
+
+
+
+    $inputPanels.each(function(i, panel){
+
+        $(panel).find('.continue').click(function(){
+            $(panel).slideUp('slow');
+            var nextPanel = $(this).attr('to');
+            console.log(nextPanel);
+            $('#'+nextPanel).slideDown('slow');
+        });
+
+
+    });
+
+
+})();
