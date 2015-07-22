a4254d844481d94b9788715aa8e2c1d84769c2d3
cdisciascio <cecidisi@gmail.com>
2015-07-22 14:52:00 +0200

    under construction - file uploader

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,9 +40,54 @@
         afterUploadAll: function(){}
     };
 
+    console.log($('#fileupload'));
     $('#fileupload').fileupload({
-        url: 'http://localhost:8888'
+        filesContainer: $('table.files')
     });
+
+
+
+/*    $('#fileupload').fileupload({
+        url: '../server/php/',
+        filesContainer: $('table.files'),
+        uploadTemplateId: null,
+        uploadTemplate: function (o) {
+            var rows = $();
+            $.each(o.files, function (index, file) {
+                var row = $('<tr class="template-upload fade">' +
+                            '<td><span class="preview"></span></td>' +
+                            '<td><p class="name"></p>' +
+                            '<div class="error"></div>' +
+                            '</td>' +
+                            '<td><p class="size"></p>' +
+                            '<div class="progress"></div>' +
+                            '</td>' +
+                            '<td>' +
+                            (!index && !o.options.autoUpload ?
+                             '<button class="start" disabled>Start</button>' : '') +
+                            (!index ? '<button class="cancel">Cancel</button>' : '') +
+                            '</td>' +
+                            '</tr>');
+                row.find('.name').text(file.name);
+                row.find('.size').text(o.formatFileSize(file.size));
+                if (file.error) {
+                    row.find('.error').text(file.error);
+                }
+                rows = rows.add(row);
+            });
+            return rows;
+        }
+    });*/
+
+    // Enable iframe cross-domain access via redirect option:
+/*    $('#fileupload').fileupload(
+        'option',
+        'redirect',
+        window.location.href.replace(
+            /\/[^\/]*$/,
+            '/cors/result.html?%s'
+        )
+    );*/
 
     /* checkboxes */
 
